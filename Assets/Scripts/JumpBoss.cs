89fe96403804c66b02d81d078bd4caf7add5b012
Marshall <mngordon@umich.edu>
2018-03-16 22:43:14 -0400

    Added Kangaroo boss

---
+++ Assets/Scripts/JumpBoss.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBoss : MonoBehaviour {
+
+    public float averageJumpTime; //average time the character will wait before jumping
+    public GameObject player; //reference to player object
+    public float maxMoveDist; //maximum distance the boss is allowed to jump
+
+    private bool isJumping = false; //bool to prevent mutliple coroutine calls
+    private Rigidbody rb;
+    private Transform tf;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        tf = GetComponent<Transform>();
+        //StartCoroutine(Jump(5.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int faceLeft = 1;
+        float xDistToPlayer = tf.position.x - player.GetComponent<Transform>().position.x;
+        //first, calculate direction of player
+        if (xDistToPlayer < 0)
+        {
+            faceLeft = 1;
+        }//boss is to the left
+        else
+        {
+            faceLeft = -1;
+        }
+        if (!isJumping)
+        {
+            //distance the boss will move towards the player
+            float moveX = Random.Range(0, Mathf.Abs(xDistToPlayer) + 2) * faceLeft;
+            StartCoroutine(Jump(moveX));
+        }
+
+
+
+	}
+
+    //coroutine that will move the boss
+    private IEnumerator Jump(float moveX)
+    {
+        isJumping = true;
+        Vector3 startPos = tf.position;
+        Vector3 endPos = new Vector3(tf.position.x + moveX, tf.position.y, tf.position.z);
+        for (float i = 0; i < averageJumpTime; i += Time.deltaTime)
+        {
+            tf.position = Vector3.Slerp(startPos,endPos, i/averageJumpTime);
+            yield return null;
+        }
+        for (float i = 0; i < averageJumpTime; i += Time.deltaTime)
+        {
+            tf.position = Vector3.Slerp(endPos, startPos, i / averageJumpTime);
+            yield return null;
+        }
+        isJumping = false;
+        yield return null;
+    }
+}
