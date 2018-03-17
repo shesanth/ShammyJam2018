9ca46a4160997132b66ff09cdea12df02fda0676
JS44 <jacobsme@umich.edu>
2018-03-16 22:06:12 -0400

    Bullets that do nothing and despawn

---
+++ Assets/DestroyOnTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTime : MonoBehaviour
+{
+
+    public float destroyTime = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(destroy());
+    }
+
+    IEnumerator destroy()
+    {
+        yield return new WaitForSeconds(destroyTime);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+    public GameObject bullet;
+
+    public float bulletSpeed = 1f;
+
+    public void DoShoot()
+    {
+        GameObject b = Instantiate(bullet, this.transform.position, Quaternion.identity);
+
+        b.GetComponent<Rigidbody>().velocity = Vector3.right * bulletSpeed;
+    }
+}
