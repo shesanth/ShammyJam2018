6fd403978940ceb53174dfcddbec60f573ca9577
Shesanth <shesanth@umich.edu>
2018-03-16 21:04:46 -0400

    add horizontal movement, player prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	Rigidbody rigid;
+
+	public float maxSpeed = 20f;
+	public float acceleration = 35f;
+	public AnimationCurve animCurve;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		rigid = this.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void FixedUpdate() {
+		Vector3 velocity = rigid.velocity;
+
+		//Horizontal Movement
+		float xInput = Input.GetAxisRaw("Horizontal");
+		float targetSpeed = xInput * maxSpeed;
+		float xDiff = targetSpeed - velocity.x;
+		float thisAcceleration = acceleration * animCurve.Evaluate(Mathf.Abs(xDiff / maxSpeed));
+		float xStep = Mathf.Sign(xDiff) *
+			Mathf.Min(Mathf.Abs(xDiff), thisAcceleration * Time.deltaTime);
+		velocity.x += xStep;
+
+		rigid.velocity = velocity;
 	}
 }
