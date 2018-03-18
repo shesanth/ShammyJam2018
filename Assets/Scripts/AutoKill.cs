34a66fcbb0dbab5e1645e15696d2835f50818d27
Marshall <mngordon@umich.edu>
2018-03-18 03:49:08 -0400

    Commit b4 merge

---
+++ Assets/Scripts/AutoKill.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoKill : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collision)
+    {
+        Enemy e = collision.GetComponent<Enemy>();
+        if(e != null)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
