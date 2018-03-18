1cf93fc4e93bc26e40e5a0c3011a7b95e7d3a614
Marshall <mngordon@umich.edu>
2018-03-18 08:14:48 -0400

    added flying level

---
+++ Assets/MoveBossUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBossUp : MonoBehaviour {
+    Rigidbody rb;
+    public float moveSpeedVariance = 0.5f;
+    public float moveSpeed = 3f;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            
+        rb.velocity = new Vector3(0,moveSpeed,0);
+    }
+}
---
+++ Assets/MoveCamerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamerUp : MonoBehaviour {
+    private Rigidbody rb;
+
+    public float moveSpeed = 5f;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        rb.velocity = new Vector3(0, moveSpeed, 0);
+    }
+}
