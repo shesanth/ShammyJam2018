0130da7ab04197e0418b9e1a36bfe9e0f3dd161c
JS44 <jacobsme@umich.edu>
2018-03-18 00:07:45 -0400

    mistake merge

--- Assets/Scripts/SpawnJumpBoss.cs
+++ Assets/Scripts/SpawnJumpBoss.cs
@@ -10,11 +10,7 @@ public class SpawnJumpBoss : MonoBehaviour {
     public GameObject boss;
     private GameObject player;
 	// Use this for initialization
-<<<<<<< HEAD
 	void Start () {
-=======
-	void Start () {
->>>>>>> b68cc1f9b7bf15f31d8a35c37c7f4de938008fd4
         player = FindObjectOfType<PlayerHealth>().gameObject;
 	}
 	
@@ -23,7 +19,6 @@ public class SpawnJumpBoss : MonoBehaviour {
 		
 	}
 
-<<<<<<< HEAD
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
@@ -36,19 +31,5 @@ public class SpawnJumpBoss : MonoBehaviour {
     public void SpawnBoss()
     {
         SceneManager.LoadScene("mars");
-=======
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == player)
-        {
-            SpawnBoss();
-        }
-    }
-
-
-    public void SpawnBoss()
-    {
-        SceneManager.LoadScene("mars");
->>>>>>> b68cc1f9b7bf15f31d8a35c37c7f4de938008fd4
     }
 }
