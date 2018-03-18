ff5d02fd4f4e58cc1f3fb33beecc04c3b08761b1
JS44 <jacobsme@umich.edu>
2018-03-18 09:57:42 -0400

    Added flavour text and ability to remove it

---
+++ Assets/Scripts/RemoveFlavourText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveFlavourText : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Submit"))
+        {
+            this.GetComponent<Canvas>().enabled = false;
+        }
+	}
+}
